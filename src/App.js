f5e132dc273a6f4d01b4b13d929f63253fda5b4b
tmayankv <106771483+tmayankv@users.noreply.github.com>
2023-12-10 00:17:55 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route
 } from 'react-router-dom'
