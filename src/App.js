778a15954be41849f1cae45c4b37b3bcf1589b57
tmayankv <106771483+tmayankv@users.noreply.github.com>
2023-12-10 00:09:53 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import News from './components/News'
 import {
   HashRouter,
-  Switch,
+  Routes,
   Route
 } from 'react-router-dom'
 
@@ -11,19 +11,16 @@ export class App extends Component {
   render() {
     return (
       <HashRouter>
-      <Navbar/>
-      <Switch>
-      
-      <Route exact path='/'><News key="general" className="d-flex" category='general'></News></Route>
-      <Route exact path='/news-app1/sports'><News key="sports" className="d-flex" category='sports'></News></Route>
-      <Route exact path='/news-app1/business'><News key="business" className="d-flex" category='business'></News></Route>
-      <Route exact path='/news-app1/technology'><News key="technology" className="d-flex" category='technology'></News></Route>
-      <Route exact path='/news-app1/science'><News key="science" className="d-flex" category='science'></News></Route>
-      <Route exact path='/news-app1/entertainment'><News key="entertainment" className="d-flex" category='entertainment'></News></Route>
-      <Route exact path='/news-app1/health'><News key="health" className="d-flex" category='health'></News></Route>
-  
-       
-      </Switch>
+        <Navbar/>
+      <Routes>
+      <Route path='/' element={<News key="general" className="d-flex" category='general' />} />
+          <Route path='/sports' element={<News key="sports" className="d-flex" category='sports' />} />
+          <Route path='/business' element={<News key="business" className="d-flex" category='business' />} />
+          <Route path='/technology' element={<News key="technology" className="d-flex" category='technology' />} />
+          <Route path='/science' element={<News key="science" className="d-flex" category='science' />} />
+          <Route path='/entertainment' element={<News key="entertainment" className="d-flex" category='entertainment' />} />
+          <Route path='/health' element={<News key="health" className="d-flex" category='health' />} />
+        </Routes>
       </HashRouter>
       
       )
