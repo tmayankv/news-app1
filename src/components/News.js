11920cf7434eae12a7979a08dc1479d73ca14e1a
tmayankv <106771483+tmayankv@users.noreply.github.com>
2023-12-10 01:05:50 +0530

    added

--- src/components/News.js
+++ src/components/News.js
@@ -68,7 +68,7 @@ loader={<div className=" text-center my-3 ">
           <div className="row text-center">
         {this.state.articles?.map((element)=> 
             {return <div className="col-md-3 my-3 d-flex justify-content-center " key={element.url}>
-       <Newsitem title={element.title===null?'':element.title.slice(0,55)} desc={element.description} img={element.urlToImage===null?"http://content.abt.com/media/images/products/sorry-no-image-available.png":element.urlToImage} name={element.source.name} date={element.publishedAt}  author={element.author} url={element.url}/>
+       <Newsitem title={element.title===null?'':element.title.slice(0,55)} desc={element.description} img={element.urlToImage===null?"https://content.abt.com/media/images/products/sorry-no-image-available.png":element.urlToImage} name={element.source.name} date={element.publishedAt}  author={element.author} url={element.url}/>
  
             </div>
                   })}    
